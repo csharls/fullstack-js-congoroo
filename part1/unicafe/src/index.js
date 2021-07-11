1c53e3097702a19b8a2483fd701f7e0755de840f
Carlos Garcia <carlossgarcia@outlook.com>
2021-07-11 14:15:57 -0600

    unicafe step 3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -27,6 +27,7 @@ const App = () => {
       <p>bad {bad}</p>
       <p>all {good+neutral+bad}</p>
       <p>average {(good-bad)/(good+neutral+bad)}</p>
+      <p>positive {good/(good+neutral+bad) *100} %</p>
     </div>
   )
 }

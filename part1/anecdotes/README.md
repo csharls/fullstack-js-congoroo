00d39da7797b3db8524eafee81da63e35591ab16
Carlos Garcia <carlossgarcia@outlook.com>
2021-07-15 16:51:29 -0600

    add anecdotes step 1

---
+++ part1/anecdotes/README.md
@@ -0,0 +1,2 @@
+# exercise anecdotes
+In this aplication, there is a button that once clicked it will show a random anecdote, a short phrase
---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const nextClickHandler = ()=>{
+    const number = randNumber()
+    setSelected(number)
+  }
+
+  const randNumber = () => {
+    const rand = Math.random()
+    return rand <= 0.5 ? Math.round(rand*10) : randNumber()
+    }
+
+  return (
+    <div>
+      { props.anecdotes[selected] }
+      <button onClick={ nextClickHandler }>next anecdote</button>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)

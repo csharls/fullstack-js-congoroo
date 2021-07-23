f3f3b93687b54d142f5030ef30a41ea02616b250
Carlos Garcia <carlossgarcia@outlook.com>
2021-07-23 16:15:09 -0600

    Exersice 2.6 phonebook

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import Course from './components/Course.js'
 
-
-
-
-
 const App = () => {
   const courses = [
     {
---
+++ part2/phonebook/README.md
@@ -0,0 +1,2 @@
+# Phonebook example app 
+In this app i am creating a phonebook using react and the hook useState

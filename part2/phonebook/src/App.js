5c53025df259357697c753bcecf7ceb487b88a15
Carlos Garcia <carlossgarcia@outlook.com>
2021-07-24 00:52:58 -0600

    Add phonebook steps 3 and 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,50 +2,73 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
+
+  const [newPhone, setNewPhone] = useState('')
+
+  const [searchText, setSearchText] = useState('')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    const duplicated = nameVerify(newName)
+    const duplicated = persons.find( p => p.name === newName)
+
     if(duplicated){
       alert(`${newName} is already added to phonebook`)
     }
     else{
       const newPerson = {
-        name: newName
+        name: newName,
+        number: newPhone
       }
       setPersons(prevPersons => prevPersons.concat(newPerson))
     }
     setNewName('')
+    setNewName('')
 
   }
 
-  const handleChange = (e) => {
+  const handleNameChange = (e) => {
     setNewName(e.target.value)
   }
 
-  const nameVerify = (name) => {
-    return persons.find( p => p.name === name)
+  const handlePhoneChange = (e) => {
+    setNewPhone(e.target.value)
+  }
+
+  const handleSearch = (e) => {
+    setSearchText(e.target.value)
   }
 
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <span>filter shown with</span> <input onChange={handleSearch} />
+      </div>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input onChange={handleChange} value={newName}/>
+          name: <input onChange={handleNameChange} value={newName}/>
+        </div>
+        <div>
+          phone: <input onChange={handlePhoneChange} value={newPhone}/>
         </div>
         <div>
           <button>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(
+      {persons.filter(
+        p=> p.name.toLowerCase().includes(searchText)
+      )
+      .map(
         ({name}) => <p key={name}>{name}</p>
       )}
     </div>
